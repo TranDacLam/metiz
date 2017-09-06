d9ba4cbe04008d44d01de281beee97af0865f74a
thuongpx <phungxuanthuong@gmail.com>
2017-09-06 10:30:06 +0700

    add template

--- websites/main/views.py
+++ websites/main/views.py
@@ -28,8 +28,10 @@ def cinox(request):
 	return render(request, 'websites/cinox.html', {})
 
 def gift_card(request):
-	
-	return render(request, 'websites/gift_card.html', {})
+	data=[{'img':'/assets/websites/images/gift_card_-_cinox_1.png','title':'Thẻ Quà Tặng - 300.000đ','price':'300.000,00 ₫','description':'Có giá trị như tiền mặt'},
+	{'img':'/assets/websites/images/gift_card_-_cinox_1.png','title':'Thẻ Quà Tặng - 500.000đ','price':'500.000,00 ₫','description':'Có giá trị như tiền mặt'}]
+
+	return render(request, 'websites/gift_card.html', {'data':data})
 
 def membership(request):
 	
@@ -79,7 +81,14 @@ def about_cinema(request):
 	return render(request, 'websites/about_cinema.html',{'data': data})
 
 def gift_card_detail(request):
-	return render(request, 'websites/gift_card_detail.html',{} )
+	terms_conditions = [{'description':'Thẻ này dùng để đổi vé xem phim hoặc thức ăn, đồ uống tại tất cả các rạp CGV, áp dụng tại quầy & trực tuyến.'},
+	{'description':'Thẻ có thẻ được nạp thêm tiền và gia hạn tại quầy hoặc trực tuyến bất cứ lúc nào với số tiền tối thiểu 300,000đ.'},
+	{'description':'Riêng với thẻ quà tặng được đăng ký trực tuyến, ngày hết hạn của tất cả thẻ trước đó sẽ được gia hạn theo ngày hết hạn của thẻ mới nhất.'},
+	{'description':'Thẻ có giá trị tích lũy điểm và tổng chi tiêu cho người sử dụng, không có giá trị tích lũy điểm và tổng chi tiêu cho người mua.'},
+	{'description':'Thẻ không được dùng để đổi sang tiền mặt hoặc mua thẻ khác.'},
+	{'description':'Nếu thẻ bị mất hoặc hư hại, giá trị sử dụng sẽ không còn hiệu lực hay cấp lại nếu không có bằng chứng xác thực hợp lệ nào.'}]
+	
+	return render(request, 'websites/gift_card_detail.html',{'terms_conditions':terms_conditions} )
 
 def cgv_online(request):
 	return render(request, 'websites/cgv_online.html', {} )
@@ -91,12 +100,24 @@ def contacts(request):
 	return render(request, 'websites/contacts.html', {})
 
 def terms_conditions(request):
-	return render(request, 'websites/terms_conditions.html', {})
+
+	data = [{'title':'1. Trách nhiệm của người sử dụng:','content':'Khi truy cập vào trang web này, bạn đồng ý chấp nhận mọi rủi ro. CGV và các bên đối tác khác không chịu trách nhiệm về bất kỳ tổn thất nào do những hậu quả trực tiếp, tình cờ hay gián tiếp; những thất thoát, chi phí (bao gồm chi phí pháp lý, chi phí tư vấn hoặc các khoản chi tiêu khác) có thể phát sinh trực tiếp hoặc gián tiếp do việc truy cập trang web hoặc khi tải dữ liệu về máy; những tổn hại gặp phải do virus, hành động phá hoại trực tiếp hay gián tiếp của hệ thống máy tính khác, đường dây điện thoại, phần cứng, phần mềm, lỗi chương trình, hoặc bất kì các lỗi nào khác; đường truyền dẫn của máy tính hoặc nối kết mạng bị chậm…'},
+	{'title':'2. Về nội dung trên trang web:','content':'Tất cả những thông tin ở đây được cung cấp cho bạn một cách trung thực như bản thân sự việc. CGV và các bên liên quan không bảo đảm, hay có bất kỳ tuyên bố nào liên quan đến tính chính xác, tin cậy của việc sử dụng hay kết quả của việc sử dụng nội dung trên trang web này. Nột dung trên website được cung cấp vì lợi ích của cộng đồng và có tính phi thương mại. Các cá nhân và tổ chức không được phếp sử dụng nội dung trên website này với mục đích thương mại mà không có sự ưng thuận của CGV bằng văn bản. Mặc dù CGV luôn cố gắng cập nhật thường xuyên các nội dung tại trang web, nhưng chúng tôi không bảo đảm rằng các thông tin đó là mới nhất, chính xác hay đầy đủ. Tất cả các nội dung website có thể được thay đổi bất kỳ lúc nào.'},
+	{'title':'3. Về bản quyền:','content':'CGV là chủ bản quyền của trang web này. Việc chỉnh sửa trang, nội dung, và sắp xếp thuộc về thẩm quyền của CGV. Sự chỉnh sửa, thay đổi, phân phối hoặc tái sử dụng những nội dung trong trang này vì bất kì mục đích nào khác được xem như vi phạm quyền lợi hợp pháp của CGV.'},
+	{'title':'4. Về việc sử dụng thông tin:','content':'Chúng tôi sẽ không sử dụng thông tin cá nhân của bạn trên website này nếu không được phép. Nếu bạn đồng ý cung cấp thông tin cá nhân, bạn sẽ được bảo vệ. Thông tin của bạn sẽ được sử dụng với mục đích, liên lạc với bạn để thông báo các thông tin cập nhật của CGV như lịch chiếu phim, khuyến mại qua email hoặc bưu điện. Thông tin cá nhân của bạn sẽ không được gửi cho bất kỳ ai sử dụng ngoài trang web CGV, ngoại trừ những mở rộng cần thiết để bạn có thể tham gia vào trang web (những nhà cung cấp dịch vụ, đối tác, các công ty quảng cáo) và yêu cầu cung cấp bởi luật pháp. Nếu chúng tôi chia sẻ thông tin cá nhân của bạn cho các nhà cung cấp dịch vụ, công ty quảng cáo, các công ty đối tác liên quan, thì chúng tôi cũng yêu cầu họ bảo vệ thông tin cá nhân của bạn như cách chúng tôi thực hiện.'},
+	{'title':'5. Về việc tải dữ liệu:','content':'Nếu bạn tải về máy những phần mềm từ trang này, thì phần mềm và các dữ liệu tải sẽ thuộc bản quyền của CGV và cho phép bạn sử dụng. Bạn không được sở hữu những phầm mềm đã tải và CGV không nhượng quyền cho bạn. Bạn cũng không được phép bán, phân phối lại, hay bẻ khóa phần mềm…'},
+	{'title':'6. Thay đổi nội dung:','content':'CGV giữ quyền thay đổi, chỉnh sửa và loại bỏ những thông tin hợp pháp vào bất kỳ thời điểm nào vì bất kỳ lý do nào.'},
+	{'title':'7. Liên kết với các trang khác: ','content':'Mặc dù trang web này có thể được liên kết với những trang khác, CGV không trực tiếp hoặc gián tiếp tán thành, tổ chức, tài trợ, đứng sau hoặc sát nhập với những trang đó, trừ phi điều này được nêu ra rõ ràng. Khi truy cập vào trang web bạn phải hiểu và chấp nhận rằng CGV không thể kiểm soát tất cả những trang liên kết với trang CGV và cũng không chịu trách nhiệm cho nội dung của những trang liên kết.'},
+	{'title':'8. Đưa thông tin lên trang web:','content':'Bạn không được đưa lên, hoặc chuyển tải lên trang web tất cả những hình ảnh, từ ngữ khiêu dâm, thô tục, xúc phạm, phỉ báng, bôi nhọ, đe dọa, những thông tin không hợp pháp hoặc những thông tin có thể đưa đến việc vi phạm pháp luật, trách nhiệm pháp lý. CGV và tất cả các bên có liên quan đến việc xây dựng và quản lý trang web không chịu trách nhiệm hoặc có nghĩa vụ pháp lý đối với những phát sinh từ nội dung do bạn tải lên trang web.'},
+	{'title':'9. Luật áp dụng:','content':'Mọi hoạt động phát sinh từ trang web có thể sẽ được phân tích và đánh giá theo luật pháp Việt Nam và toà án Tp. Hồ Chí Minh. Và bạn phải đồng ý tuân theo các điều khoản riêng của các toà án này.'}]
+	
+	return render(request, 'websites/terms_conditions.html', {'data':data})
 
 def terms_use(request):
 	return render(request, 'websites/terms_use.html', {})
 
 def payment_policy(request):
+
 	return render(request, 'websites/payment_policy.html', {})
 
 def privacy_policy(request):
