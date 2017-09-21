dc3535edff779e7c608204fb58d7c6202f92e7c5
tranlam <tdlam2105@gmail.com>
2017-09-21 16:00:06 +0700

    Change tab page news and movie page home

--- static/assets/websites/js/home.js
+++ static/assets/websites/js/home.js
@@ -59,36 +59,6 @@ $(document).ready(function() {
         } catch (err) {
             console.log(err.message);
         }
-    }
-    // (64-91) function click tab slide
-    function openMovie(evt, movie) {
-        var i, tabcontent, tablinks;
-        tabcontent = document.getElementsByClassName("tabcontent");
-        for (i = 0; i < tabcontent.length; i++) {
-            tabcontent[i].style.display = "none";
-        }
-        tablinks = document.getElementsByClassName("tablinks");
-        for (i = 0; i < tablinks.length; i++) {
-            tablinks[i].className = tablinks[i].className.replace(" active", "");
-        }
-        
-        document.getElementById(movie).style.display = "block";
-        evt.currentTarget.className += " active";
-    }
-            
-    function openCategory(evt, category) {
-        var i, tabcontent1, tablinks1;
-        tabcontent1 = document.getElementsByClassName("tabcontent1");
-        for (i = 0; i < tabcontent1.length; i++) {
-            tabcontent1[i].style.display = "none";
-        }
-        tablinks1 = document.getElementsByClassName("tablinks1");
-        for (i = 0; i < tablinks1.length; i++) {
-            tablinks1[i].className = tablinks1[i].className.replace(" active", "");
-        }
-        document.getElementById(category).style.display = "block";
-        evt.currentTarget.className += " active";
-    }
-            
+    }      
         
 });
