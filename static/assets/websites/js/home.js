70cef80e32ce243f8882fdb6f3dafaabd66623a1
huyhoang <huyhoang1996ha@gmail.com>
2017-10-03 20:14:25 +0700

    fix home template

--- static/assets/websites/js/home.js
+++ static/assets/websites/js/home.js
@@ -13,15 +13,7 @@ $(document).ready(function() {
 	        fixedContentPos: false
 		  });
     });
-    //action for div #show_more hover 
-    $('#show_more').hover(function() {
-    	$('#more').css('display', 'none');
-    	$('#more_hover').css('display', 'block');
-    }, function() {
-    	/* Stuff to do when the mouse leaves the element */
-    	$('#more').css('display', 'block');
-    	$('#more_hover').css('display', 'none');
-    });
+    //action for show map in footer 
     $('.map-trigger').click(function(event) {
         event.preventDefault();
         $('#content-map').toggle('slow/400/fast');
