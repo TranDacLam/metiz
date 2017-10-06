86873adcd4c3026dbe9bec39a4f1cd7f6558483c
huyhoang <huyhoang1996ha@gmail.com>
2017-10-06 10:41:41 +0700

    fix header

---
+++ static/assets/websites/js/header.js
@@ -0,0 +1,6 @@
+// $(document).ready(function() {
+// 	var link_account = 'account_login';          
+//         if(window.location.href.indexOf(name) > -1){
+//             $('.skip-account').css('color', '#ff5400');
+//         }
+// });
