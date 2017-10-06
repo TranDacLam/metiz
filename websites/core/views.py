5638562fe4acd86e8a35ac2ed1695c5db3489184
Phung xuan Thuong <phungthuong@vooc.vn>
2017-10-06 08:44:17 +0700

    fix film detail and home page,fix name function

--- websites/core/views.py
+++ websites/core/views.py
@@ -24,8 +24,9 @@ def showing(request):
         return render(request, 'websites/showing.html', {'data_showing': data_showing})
     except Exception, e:
         print "Error: ", e
+        return HttpResponse(status=500)
 
-def comingsoon(request):
+def coming_soon(request):
     try:
         # get data moving comingsoon
         data = Movie.objects.filter(release_date__gte=datetime.now()).order_by(
@@ -33,6 +34,7 @@ def comingsoon(request):
         return render(request, 'websites/coming_soon.html', {'data': data})
     except Exception, e:
         print "Error: ", e
+        return HttpResponse(status=500)
 
 
 def film_detail(request, id):
@@ -46,18 +48,20 @@ def film_detail(request, id):
         # count sum rating in comments
         rating__sum = comments.aggregate(Sum('rating')).get('rating__sum')
 
-        dic = []
+        total_percent = []
         for i in range(1, 6):
             # lay tong so sao cung loai (1 sao, 2 sao,...5 sao) trong danh gia
             count = comments.filter(rating=i).aggregate(
                 Sum('rating')).get('rating__sum')
             # dua tong so sao tung loai vao mang
-            dic.append(count)
-        return render(request, 'websites/film_detail.html', {'dic': dic, 'count': count, 'rating__sum': rating__sum, 'film_detail': film_detail, 'comments': comments, 'rating__avg': rating__avg})
-    except Movie.ObjectDoesNotExist:
-        print "Page not found !"
+            total_percent.append(count)
+        return render(request, 'websites/film_detail.html', {'total_percent': total_percent, 'count': count, 'rating__sum': rating__sum, 'film_detail': film_detail, 'comments': comments, 'rating__avg': rating__avg})
+    except Movie.DoesNotExist, e:
+        print "Error Movie : %s"%e
+        return HttpResponse(status=404)
     except Exception, e:
         print "Error: ", e
+        return HttpResponse(status=500)
 
 def news(request):
     try:
@@ -65,8 +69,8 @@ def news(request):
         news = NewOffer.objects.all().order_by('priority', 'apply_date')
         return render(request, 'websites/news.html', {'news': news})
     except Exception, e:
-        print "Error: ", e
-
+        print "Error: %s"%e
+        return HttpResponse(status=500)
 
 def new_detail(request, id):
     try:
@@ -87,16 +91,21 @@ def getCinemaTechnologyByName(request, name):
         return render(request, 'websites/cinema_technology.html', {'technology': technology, 'allTechnology': allTechnology})
     except Exception, e:
         print "Error: ", e
+        return HttpResponse(status=500)
 
 def home(request):
     try:
         # banner on home page
         result = {}
         banners = Banner.objects.filter(is_show=True).order_by('position')
-        banner_position = {}
-        for item in banners:
-            banner_position[item.position] = item
-        result["banners"] = banner_position
+        
+        position_1 = banners[0].position
+        image_1 = banners[0].image
+        sub_url_1 = banners[0].sub_url
+
+        position_2 = banners[1].position
+        image_2 = banners[1].image
+        sub_url_2 = banners[1].sub_url
 
         # phim dang chieu
         movie_showing = Movie.objects.filter(
@@ -106,11 +115,13 @@ def home(request):
             release_date__gte=datetime.now()).order_by('priority', 'release_date')
         # slide banner home page
         data_slide = SlideShow.objects.all()
-        return render(request, 'websites/home.html', {'data_slide': data_slide, 'movie_soon': movie_soon, 'movie_showing': movie_showing, 'result': result})
-    
+        return render(request, 'websites/home.html', {'image_1':image_1,'image_2':image_2,'sub_url_1':sub_url_1,'sub_url_2':sub_url_2,'position_1':position_1,'position_2':position_2,'banners':banners,'data_slide': data_slide, 'movie_soon': movie_soon, 'movie_showing': movie_showing})
+    except Movie.DoesNotExist, e:
+        print "Error Movie : %s"%e
+        return HttpResponse(status=404)
     except Exception, e:
         print "Error: ", e
-
+        return HttpResponse(status=500)
 
 def get_post(request):
     try:
