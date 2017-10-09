9c3af19b8fa79cdff11689e4d22e2492f3da3fa9
huyhoang <huyhoang1996ha@gmail.com>
2017-10-09 17:44:01 +0700

    add templatetags and remove data_schedule

--- websites/core/views.py
+++ websites/core/views.py
@@ -7,69 +7,6 @@ from datetime import *
 from django.db.models import Avg, Sum, Count
 from django.http import HttpResponse, JsonResponse
 
-data_schedule= [{
-    'date':'2017-10-27',
-    'film':[
-        {'name': 'Yeu Di Dung So',
-        'schedule': [
-        {'from': '8:35', 'to': '10:35', 'room': 119,'name_room': 'Scm01'},
-        {'from': '18:35', 'to': '21:35', 'room': 119,'name_room': 'Scm01'},
-        {'from': '21:35', 'to': '0:35', 'room': 119,'name_room': 'Scm01'},
-        ] },
-        {'name': 'The gioi nay la cua bo may',
-        'schedule': [
-        {'from': '18:35', 'to': '21:35', 'room': 119,'name_room': 'Scm01'},
-        {'from': '18:35', 'to': '21:35', 'room': 119,'name_room': 'Scm01'},
-        {'from': '18:35', 'to': '21:35', 'room': 119,'name_room': 'Scm01'},
-        ] },
-        {'name': 'Chu he IT',
-        'schedule': [
-        {'from': '18:35', 'to': '21:35', 'room': 119,'name_room': 'Scm01'},
-        {'from': '18:35', 'to': '21:35', 'room': 119,'name_room': 'Scm01'},
-        ]}
-    ]},
-    {
-    'date':'2017-10-28',
-    'film':[
-        {'name': 'Yeu Di Dung So',
-        'schedule': [
-        {'from': '8:35', 'to': '10:35', 'room': 119,'name_room': 'Scm01'},
-        {'from': '18:35', 'to': '21:35', 'room': 119,'name_room': 'Scm01'},
-        
-        ] },
-        {'name': 'The gioi nay la cua bo may',
-        'schedule': [
-        {'from': '18:35', 'to': '21:35', 'room': 119,'name_room': 'Scm01'},
-        {'from': '18:35', 'to': '21:35', 'room': 119,'name_room': 'Scm01'},
-        ] },
-        {'name': 'Chu he IT',
-        'schedule': [
-        {'from': '18:35', 'to': '21:35', 'room': 119,'name_room': 'Scm01'},
-        {'from': '18:35', 'to': '21:35', 'room': 119,'name_room': 'Scm01'},
-        ]}
-    ]},
-    {
-    'date':'2017-10-29',
-    'film':[
-        {'name': 'Yeu Di Dung So',
-        'schedule': [
-        {'from': '8:35', 'to': '10:35', 'room': 119,'name_room': 'Scm01'},
-        
-        ] },
-        {'name': 'The gioi nay la cua bo may',
-        'schedule': [
-        {'from': '18:35', 'to': '21:35', 'room': 119,'name_room': 'Scm01'},
-        {'from': '18:35', 'to': '21:35', 'room': 119,'name_room': 'Scm01'},
-        {'from': '21:35', 'to': '0:35', 'room': 119,'name_room': 'Scm01'},
-        ] },
-        {'name': 'Chu he IT',
-        'schedule': [
-        {'from': '18:35', 'to': '21:35', 'room': 119,'name_room': 'Scm01'},
-        {'from': '18:35', 'to': '21:35', 'room': 119,'name_room': 'Scm01'},
-        ]}
-    ]},
-    ]
-data_celendar = {'last' : range(15,27),'present':range(27,30), 'future': range(1,15) }
 def custom_404(request):
     return render(request, 'websites/errors/404.html', {}, status=404)
 
@@ -80,13 +17,10 @@ def custom_500(request):
 
 def showing(request):
     try:
-         # data for modal
-        data_modal= data_schedule
-        data_date=data_celendar
         # get data movie showing
         data_showing = Movie.objects.filter(
             release_date__lte=datetime.now(), is_draft=False).order_by('priority', 'release_date')
-        return render(request, 'websites/showing.html', {'data_modal': data_modal, 'data_date':data_date,'data_showing': data_showing})
+        return render(request, 'websites/showing.html', {'data_showing': data_showing})
     except Exception, e:
         print "Error: ", e
         return HttpResponse(status=500)
@@ -94,13 +28,10 @@ def showing(request):
 
 def coming_soon(request):
     try:
-         # data for modal
-        data_modal= data_schedule
-        data_date=data_celendar
         # get data moving comingsoon
         data = Movie.objects.filter(release_date__gte=datetime.now(), is_draft=False).order_by(
             'priority', 'release_date')
-        return render(request, 'websites/coming_soon.html', {'data_modal': data_modal, 'data_date':data_date,'data': data})
+        return render(request, 'websites/coming_soon.html', {'data': data})
     except Exception, e:
         print "Error: ", e
         return HttpResponse(status=500)
@@ -108,9 +39,6 @@ def coming_soon(request):
 
 def film_detail(request, id):
     try:
-         # data for modal
-        data_modal= data_schedule
-        data_date=data_celendar
         # get film detail by id
         film_detail = Movie.objects.get(pk=id, is_draft=False)
         # filter comments of film detail by movie_id
@@ -127,7 +55,7 @@ def film_detail(request, id):
                 Sum('rating')).get('rating__sum')
             # dua tong so sao tung loai vao mang
             total_percent.append(count)
-        return render(request, 'websites/film_detail.html', {'data_modal': data_modal, 'data_date':data_date, 'total_percent': total_percent, 'count': count, 'rating__sum': rating__sum, 'film_detail': film_detail, 'comments': comments, 'rating__avg': rating__avg})
+        return render(request, 'websites/film_detail.html', { 'total_percent': total_percent, 'count': count, 'rating__sum': rating__sum, 'film_detail': film_detail, 'comments': comments, 'rating__avg': rating__avg})
     except Movie.DoesNotExist, e:
         print "Error Movie : %s" % e
         return HttpResponse(status=404)
@@ -138,12 +66,10 @@ def film_detail(request, id):
 
 def news(request):
     try:
-         # data for modal
-        data_modal= data_schedule
-        data_date=data_celendar
+
         # get news order by priority and apply_date
         news = NewOffer.objects.all().order_by('priority', 'apply_date')
-        return render(request, 'websites/news.html', {'data_modal': data_modal, 'data_date':data_date ,'news': news})
+        return render(request, 'websites/news.html', {'news': news})
     except Exception, e:
         print "Error: %s" % e
         return HttpResponse(status=500)
@@ -151,12 +77,9 @@ def news(request):
 
 def new_detail(request, id):
     try:
-         # data for modal
-        data_modal= data_schedule
-        data_date=data_celendar
         # get news detail by id
         new = NewOffer.objects.get(pk=id)
-        return render(request, 'websites/new_detail.html', {'data_modal': data_modal, 'data_date':data_date, 'new': new})
+        return render(request, 'websites/new_detail.html', { 'new': new})
     except NewOffer.DoesNotExist, e:
         print "Error new_detail : %s" % e
         return HttpResponse(status=404)
@@ -167,13 +90,10 @@ def new_detail(request, id):
 
 def technology_detail(request, name):
     try:
-         # data for modal
-        data_modal= data_schedule
-        data_date=data_celendar
         # get technology detail by name
         allTechnology = CenimaTechnology.objects.all()
         technology = allTechnology.get(name=name)
-        return render(request, 'websites/cinema_technology.html', {'data_modal': data_modal, 'data_date':data_date,  'technology': technology, 'allTechnology': allTechnology})
+        return render(request, 'websites/cinema_technology.html', { 'technology': technology, 'allTechnology': allTechnology})
     except Exception, e:
         print "Error: ", e
         return HttpResponse(status=500)
@@ -181,9 +101,6 @@ def technology_detail(request, name):
 
 def home(request):
     try:
-        # data for modal
-        data_modal= data_schedule
-        data_date=data_celendar
         # banner on home page
         result = {}
         banners = Banner.objects.filter(is_show=True).order_by('position')
@@ -202,8 +119,7 @@ def home(request):
             release_date__gte=datetime.now(), is_draft=False).order_by('priority', 'release_date')
         # slide banner home page
         data_slide = SlideShow.objects.filter(is_draft=False)
-        print(data_date)
-        return render(request, 'websites/home.html', { 'data_modal': data_modal, 'data_date':data_date,  'position_1': position_1, 'position_2': position_2, 'data_slide': data_slide, 'movie_soon': movie_soon, 'movie_showing': movie_showing})
+        return render(request, 'websites/home.html', {  'position_1': position_1, 'position_2': position_2, 'data_slide': data_slide, 'movie_soon': movie_soon, 'movie_showing': movie_showing})
     except Movie.DoesNotExist, e:
         print "Error Movie : %s" % e
         return HttpResponse(status=404)
