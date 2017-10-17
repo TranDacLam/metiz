39bc0af1009898e801bbc65222ab84e20eb6022e
tiendang <tiendang@tiendangs-iMac.local>
2017-10-17 13:50:49 +0700

    fix slick import js

--- websites/config/setting_local.py
+++ websites/config/setting_local.py
@@ -19,3 +19,4 @@ VNPAY_PAYMENT_URL = 'http://sandbox.vnpayment.vn/paymentv2/vpcpay.html'  # get f
 VNPAY_API_URL = 'http://sandbox.vnpayment.vn/merchant_webapi/merchant.html'
 VNPAY_TMN_CODE = 'ZJLNTI3D'  # Website ID in VNPAY System, get from config
 VNPAY_HASH_SECRET_KEY = 'LAJRLYXKVBAOGGQIRTKTNRYCWQOVJBZR'  # Secret key for create checksum,get from config
+CINESTAR_SERECT_KEY = 'df733de5b7394530835a2d61ce20e220'
