a6fdc90827cc86aab5d5aa3cde5dc42e6f212079
tranlam <tdlam2105@gmail.com>
2017-10-23 16:28:12 +0700

    Fix booking with type seat

--- static/assets/websites/js/booking.js
+++ static/assets/websites/js/booking.js
@@ -1,6 +1,6 @@
-var price = 10; //price
 // data example
 var list_booking = {"List":[{"ID":"11984242","NAME":"A01","STATUS":"True","X":"198","Y":"63","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984249","NAME":"A02","STATUS":"True","X":"264","Y":"63","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984256","NAME":"A03","STATUS":"False","X":"330","Y":"63","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984266","NAME":"A04","STATUS":"False","X":"396","Y":"63","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984274","NAME":"A05","STATUS":"False","X":"462","Y":"63","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984282","NAME":"A06","STATUS":"False","X":"528","Y":"63","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984289","NAME":"A07","STATUS":"False","X":"594","Y":"63","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984296","NAME":"A08","STATUS":"False","X":"660","Y":"63","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984304","NAME":"A10","STATUS":"False","X":"726","Y":"63","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984237","NAME":"B01","STATUS":"False","X":"132","Y":"126","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984243","NAME":"B02","STATUS":"False","X":"198","Y":"126","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984250","NAME":"B03","STATUS":"False","X":"264","Y":"126","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984257","NAME":"B04","STATUS":"False","X":"330","Y":"126","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984267","NAME":"B05","STATUS":"False","X":"396","Y":"126","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984275","NAME":"B06","STATUS":"False","X":"462","Y":"126","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984283","NAME":"B07","STATUS":"False","X":"528","Y":"126","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984290","NAME":"B08","STATUS":"False","X":"594","Y":"126","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984297","NAME":"B09","STATUS":"False","X":"660","Y":"126","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984305","NAME":"B10","STATUS":"True","X":"726","Y":"126","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984238","NAME":"C01","STATUS":"False","X":"132","Y":"189","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984244","NAME":"C02","STATUS":"False","X":"198","Y":"189","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984251","NAME":"C03","STATUS":"False","X":"264","Y":"189","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984258","NAME":"C04","STATUS":"False","X":"330","Y":"189","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984268","NAME":"C05","STATUS":"False","X":"396","Y":"189","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984276","NAME":"C06","STATUS":"False","X":"462","Y":"189","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984284","NAME":"C07","STATUS":"False","X":"528","Y":"189","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984291","NAME":"C08","STATUS":"False","X":"594","Y":"189","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984298","NAME":"C09","STATUS":"False","X":"660","Y":"189","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984306","NAME":"C10","STATUS":"False","X":"726","Y":"189","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984239","NAME":"D01","STATUS":"False","X":"132","Y":"252","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984245","NAME":"D02","STATUS":"False","X":"198","Y":"252","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984252","NAME":"D03","STATUS":"True","X":"264","Y":"252","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984259","NAME":"D04","STATUS":"False","X":"330","Y":"252","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984269","NAME":"D05","STATUS":"False","X":"396","Y":"252","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984277","NAME":"D06","STATUS":"False","X":"462","Y":"252","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984285","NAME":"D07","STATUS":"False","X":"528","Y":"252","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984292","NAME":"D08","STATUS":"False","X":"594","Y":"252","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984299","NAME":"D09","STATUS":"False","X":"660","Y":"252","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984307","NAME":"D11","STATUS":"False","X":"726","Y":"252","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984240","NAME":"E01","STATUS":"False","X":"132","Y":"315","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984246","NAME":"E02","STATUS":"False","X":"198","Y":"315","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984253","NAME":"E03","STATUS":"False","X":"264","Y":"315","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984260","NAME":"E04","STATUS":"False","X":"330","Y":"315","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984270","NAME":"E05","STATUS":"False","X":"396","Y":"315","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984278","NAME":"E06","STATUS":"False","X":"462","Y":"315","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984286","NAME":"E07","STATUS":"False","X":"528","Y":"315","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984293","NAME":"E08","STATUS":"False","X":"594","Y":"315","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984300","NAME":"E09","STATUS":"False","X":"660","Y":"315","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984308","NAME":"E10","STATUS":"False","X":"726","Y":"315","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984241","NAME":"F01","STATUS":"False","X":"132","Y":"378","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984247","NAME":"F02","STATUS":"False","X":"198","Y":"378","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984254","NAME":"F03","STATUS":"False","X":"264","Y":"378","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984261","NAME":"F04","STATUS":"False","X":"330","Y":"378","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984271","NAME":"F05","STATUS":"False","X":"396","Y":"378","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984279","NAME":"F06","STATUS":"False","X":"462","Y":"378","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984287","NAME":"F07","STATUS":"False","X":"528","Y":"378","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984294","NAME":"F08","STATUS":"False","X":"594","Y":"378","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984301","NAME":"F09","STATUS":"False","X":"660","Y":"378","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984309","NAME":"F10","STATUS":"False","X":"726","Y":"378","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984262","NAME":"G01","STATUS":"False","X":"330","Y":"441","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984272","NAME":"G02","STATUS":"False","X":"396","Y":"441","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984280","NAME":"G03","STATUS":"False","X":"462","Y":"441","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984288","NAME":"G04","STATUS":"False","X":"528","Y":"441","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984295","NAME":"G05","STATUS":"False","X":"594","Y":"441","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984302","NAME":"G06","STATUS":"False","X":"660","Y":"441","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984263","NAME":"H01","STATUS":"False","X":"330","Y":"504","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984273","NAME":"H02","STATUS":"False","X":"396","Y":"504","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984281","NAME":"H03","STATUS":"False","X":"462","Y":"504","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984303","NAME":"H04","STATUS":"False","X":"660","Y":"504","W":"129","H":"60","COLOR":"Fuchsia","TYPE_SEAT_ID":"30","TYPE_SEAT_NAME":"ĐÔI","PRICE":""},{"ID":"11984264","NAME":"I03","STATUS":"False","X":"330","Y":"567","W":"129","H":"60","COLOR":"Fuchsia","TYPE_SEAT_ID":"30","TYPE_SEAT_NAME":"ĐÔI","PRICE":""},{"ID":"11984310","NAME":"I08","STATUS":"False","X":"660","Y":"567","W":"63","H":"60","COLOR":"ffff8000","TYPE_SEAT_ID":"29","TYPE_SEAT_NAME":"ĐƠN","PRICE":""},{"ID":"11984248","NAME":"J04","STATUS":"False","X":"198","Y":"630","W":"129","H":"60","COLOR":"Fuchsia","TYPE_SEAT_ID":"30","TYPE_SEAT_NAME":"ĐÔI","PRICE":""},{"ID":"11984265","NAME":"J07","STATUS":"False","X":"330","Y":"630","W":"129","H":"60","COLOR":"Fuchsia","TYPE_SEAT_ID":"30","TYPE_SEAT_NAME":"ĐÔI","PRICE":""}]}
+var TYPE_SEAT_ID = {"List":[{"ID":"30","NAME":"ĐÔI"},{"ID":"29","NAME":"ĐƠN"},{"ID":"32","NAME":"LD"},{"ID":"31","NAME":"VIP"}]};
 $(document).ready(function() {
     var $cart = $('#selected-seats'), //Sitting Area
     $counter = $('#counter'), //Votes
@@ -15,11 +15,31 @@ $(document).ready(function() {
     var objSeat = [];
     var mapArr = [];
     var mapRow = [];
+    var number;
+
+    function typeSeatBooking(choice){
+        var type_seat;
+        switch(choice) {
+            case "30": 
+                type_seat = "c";
+                break;
+            case "31": 
+                type_seat = "v";
+                break;
+            case "32": 
+                type_seat = "l";
+                break;
+            default: 
+                type_seat = "a";
+        }
+        return type_seat;
+    }
 
     // array object with name, value form list_booking NAME
     for(i=0; i < objBooking.length; i++){
         if(objBooking[i].NAME.substring(0,1) == sameStr){
             objSeat[iArr].value.push(parseInt(objBooking[i].NAME.substring(1,3)));
+            objSeat[iArr].type_seat.push(typeSeatBooking(objBooking[i].TYPE_SEAT_ID));
         }else{
             iArr++;
             //number max of row
@@ -29,7 +49,8 @@ $(document).ready(function() {
             sameStr = rowNaming[iArr] = objBooking[i].NAME.substring(0,1);
             objSeat[iArr] = {
                 'name':objBooking[i].NAME.substring(0,1),
-                'value':[parseInt(objBooking[i].NAME.substring(1,3))]
+                'value':[parseInt(objBooking[i].NAME.substring(1,3))],
+                'type_seat':[typeSeatBooking(objBooking[i].TYPE_SEAT_ID)]
             };
         }
 
@@ -45,6 +66,8 @@ $(document).ready(function() {
         }
     }
 
+    console.log(objSeat);
+
     // Range 
     function numberRange (start, end) {
         return new Array(end - start).fill().map((d, i) => i + start);
@@ -54,12 +77,15 @@ $(document).ready(function() {
 
     // get array map seat Charts
     for(i=0; i< objSeat.length; i++){
+        number = 0;
         $.grep(arrColumns, function(el) {
             if ($.inArray(el, objSeat[i].value) == -1){
                 mapRow[el-1] = "_";
+                number--;
             }else{
-                mapRow[el-1] = objSeat[i].name;
+                mapRow[el-1] = objSeat[i].type_seat[number];
             }
+            number++;
         });
         mapArr[i] = mapRow.join('');
     }
@@ -74,12 +100,35 @@ $(document).ready(function() {
                 return row+column;
             }
         },
+        seats: { //Definition seat property
+            a: {
+                price   : 10
+            },
+            v: {
+                price   : 100,
+                classes : 'seat-vip', 
+                category: 'vip'
+            },
+            c: {
+                price   : 40,
+                classes : 'seat-couple', 
+                category: 'couple'
+            },
+            l: {
+                price   : 40,
+                classes : 'seat-ld', 
+                category: 'LD'
+            }                  
+        },
         legend : { //Definition legend
             node : $('#legend'),
             items : [
-                [ 'a', 'available',   'Ghế trống' ],
                 [ 'a', 'unavailable', 'Ghế đã có người đặt'],
-                [ 'a', 'selected', 'Ghế đang chọn']
+                [ 'a', 'selected', 'Ghế đang chọn'],
+                [ 'a', 'available',   'Ghế trống' ],
+                [ 'v', 'available',   'Ghế VIP' ],
+                [ 'c', 'available',   'Ghế couple' ],
+                [ 'l', 'available',   'Ghế cao cấp' ]
             ]
         },
         click: function () { //Click event
@@ -91,14 +140,14 @@ $(document).ready(function() {
                     .appendTo($cart);
 
                 $counter.text(sc.find('selected').length+1);
-                $total.text(recalculateTotal(sc)+price);
+                $total.text(recalculateTotal(sc)+this.data().price);
 
                 return 'selected';
             } else if (this.status() == 'selected') { //Checked
                     //Update Number
                     $counter.text(sc.find('selected').length-1);
                     //update totalnum
-                    $total.text(recalculateTotal(sc)-price);
+                    $total.text(recalculateTotal(sc)-this.data().price);
 
                     //Delete reservation
                     $('#cart-item-'+this.settings.id).remove();
@@ -119,7 +168,7 @@ $(document).ready(function() {
 function recalculateTotal(sc) {
     var total = 0;
     sc.find('selected').each(function () {
-        total += price;
+        total += this.data().price;
     });
 
     return total;
