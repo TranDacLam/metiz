d9cb3aac9efa3a6e7f4c2345ce289744d72a52eb
tiendang <tiendang@tiendangs-iMac.local>
2017-10-25 14:43:33 +0700

    add action get seats

--- websites/booking/urls.py
+++ websites/booking/urls.py
@@ -4,4 +4,5 @@ import views
 
 urlpatterns = [
     url(r'^movie/show/times$', views.get_movie_show_time, name='movie-show-time-by-date'),
+    url(r'^movie/seats$', views.get_seats, name='movie-seats'),
 ]
--- websites/booking/views.py
+++ websites/booking/views.py
@@ -1,9 +1,15 @@
+
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.utils import timezone
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 from booking.models import MovieSync
 import json
 import ast
+import urllib
+import urllib2
+import requests
 
 
 def get_movie_show_time(request):
@@ -37,14 +43,52 @@ def get_movie_show_time(request):
                 # Check key not in result then create dictionary create new key
                 # with data is list empty
                 if item["MOVIE_ID"] not in result:
-                    result[item["MOVIE_ID"]] = []
+                    result[item["MOVIE_ID"]] = {"times": [], "id_showtime": item[
+                        "ID"], "movie_id": item["MOVIE_ID"]}
 
                 # Check time showing greater than currnet hour
                 if int(item["TIME"].split(':')[0]) >= current_date.hour:
-                    result[item["MOVIE_ID"]].append(item["TIME"])
+                    result[item["MOVIE_ID"]]["times"].append(item["TIME"])
 
         return JsonResponse(result)
 
     except Exception, e:
         print "Error get_movie_show_time : %s" % e
         return HttpResponse(status=500)
+
+
+def get_seats(request):
+    try:
+        """
+            Get seats by show times id
+        """
+        result = {"List": []}
+        id_server = request.GET.get('id_server', 1)
+
+        if 'id_showtime' in request.GET:
+            url_show_time = settings.BASE_URL_CINESTAR + "/getSeats"
+            values = {
+                "id_ShowTimes": request.GET['id_showtime'],
+                "id_Server": id_server,
+                "Secret": settings.CINESTAR_SERECT_KEY
+            }
+            request = urllib2.Request(url_show_time, data=urllib.urlencode(values),
+                                      headers={'Content-Type': 'application/x-www-form-urlencoded'})
+            resp = urllib2.urlopen(request)
+            # handle decoding json
+            try:
+                result = json.loads(resp.read())
+
+            except ValueError as e:
+                print "Error convert json : %s" % e
+                return JsonResponse({"code": 500, "message": _("Handle data error.")}, status=500)
+
+            return JsonResponse(result)
+        else:
+            error = {"code": 500, "message": _("Fields id_showtime and id_server is required."),
+                     "fields": "id_showtime, id_server"}
+            return JsonResponse(error, status=500)
+
+    except Exception, e:
+        print "Error get_seats : %s" % e
+        return JsonResponse({"code": 500, "message": _("Internal Server Error. Please contact administrator.")}, status=500)
