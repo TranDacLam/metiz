a254cae78085408f4f0419153b7ed47628aafdd2
tiendang <tiendang@tiendangs-iMac.local>
2017-10-26 11:20:38 +0700

    add action verify seatc choice

--- websites/booking/urls.py
+++ websites/booking/urls.py
@@ -5,4 +5,5 @@ import views
 urlpatterns = [
     url(r'^movie/show/times$', views.get_movie_show_time, name='movie-show-time-by-date'),
     url(r'^movie/seats$', views.get_seats, name='movie-seats'),
+    url(r'^verify/seats$', views.check_seats, name='verify-seats'),
 ]
