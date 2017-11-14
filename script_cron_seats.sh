423bb95745965f817b25eda92ae1d1294d6acb05
tiendang <tiendang@tiendangs-iMac.local>
2017-11-14 11:28:02 +0700

    add shell script run cron jobs

--- script_cron_seats.sh
+++ script_cron_seats.sh
@@ -1,3 +1,3 @@
 #! /bin/bash    
 source /home/adminvn/envs_root/metiz_env/bin/activate
-python /home/adminvn/sites/metiz/websites/manage.py clear_seats
+/home/adminvn/envs_root/metiz_env/bin/python /home/adminvn/sites/metiz/websites/manage.py clear_seats
