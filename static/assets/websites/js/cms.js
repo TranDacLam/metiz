24df57d304415cf97963eb3caa8acf884ae91c3a
tranlam <tdlam2105@gmail.com>
2017-11-14 17:36:22 +0700

    Fix css contact, film, modal, passowrd, footer

--- static/assets/websites/js/cms.js
+++ static/assets/websites/js/cms.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     $('.popup-gallery').magnificPopup({
         delegate: 'a',
         type: 'image',
-        tLoading: 'Loading image #%curr%...',
+        tLoading: 'Đang tải ảnh #%curr%...',
         mainClass: 'mfp-img-mobile',
         gallery: {
             enabled: true,
@@ -11,10 +11,7 @@ $(document).ready(function() {
             preload: [0,1] // Will preload 0 - before current, and 1 after the current image
         },
         image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-            titleSrc: function(item) {
-                return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-            }
+            tError: 'Lỗi tải ảnh.'
         }
     });
 
