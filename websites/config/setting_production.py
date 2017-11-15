7537feccb17523845a9a8a11c29d4cefb532422f
tiendang <tiendang@tiendangs-iMac.local>
2017-11-15 11:44:17 +0700

    add config url cinema pos and fb app id

--- websites/config/setting_production.py
+++ websites/config/setting_production.py
@@ -15,9 +15,27 @@ DATABASES = {
 
 }
 
+# Default Email Contact
+DEFAULT_TO_ADMIN_EMAIL = "thaophan@vooc.vn"
+
 # VNPAY CONFIG
-VNPAY_RETURN_URL = 'http://metiz.dht:8000/payment_return'  # get from config
+VNPAY_RETURN_URL = 'http://metiz.vn/payment_return'  # get from config
 VNPAY_PAYMENT_URL = 'http://sandbox.vnpayment.vn/paymentv2/vpcpay.html'  # get from config
 VNPAY_API_URL = 'http://sandbox.vnpayment.vn/merchant_webapi/merchant.html'
-VNPAY_TMN_CODE = 'ZJLNTI3D'  # Website ID in VNPAY System, get from config
-VNPAY_HASH_SECRET_KEY = 'LAJRLYXKVBAOGGQIRTKTNRYCWQOVJBZR'  # Secret key for create checksum,get from config
+VNPAY_TMN_CODE = 'HELIOKP1'  # Website ID in VNPAY System, get from config
+VNPAY_HASH_SECRET_KEY = 'YTDBTUZONRERICMBLYIRTRTEJDPCZDFK'  # Secret key for create checksum,get from config
+
+
+# POS Cinestar config
+CINESTAR_SERECT_KEY = 'df733de5b7394530835a2d61ce20e220'
+BASE_URL_CINESTAR = 'http://113.176.107.20:8080/helio.asmx'
+
+# SMS Config
+SMS_BRAND = "HelioCenter"
+SMS_USER = "heliocenter"
+SMS_PASSWORD = "truyenthonghelio"
+SMS_KEY = "VNFPT123BLUESEA1"
+SMS_KEY_IV = "154dxc1scfzzad21"
+SMS_URL = "http://ws.ctnet.vn/servicectnet.asmx?op=sendsms"
+
+FB_APP_ID = '156569091618914'
