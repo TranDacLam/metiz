03b48e85db0b810d4043a528271495b76ed96fde
trangle <trangle@vooc.vn>
2017-11-17 14:20:39 +0700

    Change reponsive footer

--- static/assets/websites/js/footer.js
+++ static/assets/websites/js/footer.js
@@ -10,3 +10,8 @@ $(document).ready(function() {
        
     });
 });
+$( window ).resize(function() {
+    var container_width = $('#pageFacebook').width();    
+    $('#pageFacebook').html('<div class="fb-page" data-href="https://www.facebook.com/metizcinema" data-width="' + container_width + '" data-height="210" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/metizcinema"><a href="https://www.facebook.com/metizcinema">Metiz Cinema</a></blockquote></div></div>');
+    FB.XFBML.parse();    
+});
