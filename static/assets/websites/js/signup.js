9436248b175f424ff353a78a2e39dd34558f487f
trangle <trangle@vooc.vn>
2017-11-20 15:47:37 +0700

    Prevent to enter 0 on field phone when having +84 before

--- static/assets/websites/js/signup.js
+++ static/assets/websites/js/signup.js
@@ -298,3 +298,31 @@ $(document).ready(function() {
 	}
 	
 });
+
+// Can enter 0 number at the end or middle but not at the geginning.
+$(document).ready(function() {
+//For numeric
+$(".textPhone").keydown(function(event) {
+    // Allow only backspace and delete
+    if ( event.keyCode == 46 || event.keyCode == 8) {
+        // let it happen, don't do anything
+       
+    }
+    else {
+        // Ensure that it is a number and stop the keypress
+        if ((event.keyCode !==9) && (event.keyCode < 48 || event.keyCode > 57 )) {
+            event.preventDefault(); 
+        }   
+            else{
+             
+          if($.trim($(this).val()) =='')
+        {
+            if(event.keyCode == 48){
+            event.preventDefault(); 
+            }
+        }
+                
+        }
+    }
+});
+});
