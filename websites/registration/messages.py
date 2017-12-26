e7b0e0427da487959f2e043c1329aedfdef27b5a
diemnguyen <diemnguyen@vooc.vn>
2017-12-26 15:56:18 +0700

    fix bugs

--- websites/registration/messages.py
+++ websites/registration/messages.py
@@ -4,4 +4,3 @@ EMAIL_NOT_EXIST = _("Email does not exits in system. Please check again.")
 PASSWORD_WRONG = _("Your password wrong. Please enter again.")
 INACTIVE = _("Your account not active. Please check email and active account.")
 USER_ACTIVE = _('User is active. Please login again.')
-Email này đã được kích hoạt với tài khoản khác. Vui lòng nhập lại
