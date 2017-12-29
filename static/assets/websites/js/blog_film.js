caa8ded88db9d94bc262c5677da5ae9d6968a801
trangle <trangle@vooc.vn>
2017-12-29 14:28:31 +0700

    Filter blog-film

--- static/assets/websites/js/blog_film.js
+++ static/assets/websites/js/blog_film.js
@@ -1,24 +1,14 @@
 $(document).ready(function() {
     //Filter News
 	$('select#blog-filter').change(function() {
-		var filter = $(this).val()
-		filterList(filter);
+		var filter = $(this).val();
+		$.ajax({
+			type: "POST",
+			url: "/blog/",
+			data: {'filter': filter},
+			success: function(data) {
+				console.log($(".blog-custom>article").html(data));
+			}
+		});
 	});
-
-	//News filter function
-	function filterList(value) {
-		var list = $(".blog-custom .blog-content");
-		$(list).fadeOut("fast");
-		if (value == "getNewBlog") {
-			$(".blog-custom").find("article").each(function (i) {
-				$(this).delay(100).slideDown("fast");
-			});
-		} else {
-			//Notice this *=" <- This means that if the data-category contains multiple options, it will find them
-			//Ex: data-category="Cat1, Cat2"
-			$(".blog-custom").find("article[data-category*=" + value + "]").each(function (i) {
-				$(this).delay(100).slideDown("fast");
-			});
-		}
-	}
 });
