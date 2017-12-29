3f2388883ff275521a3d54b973fa6d0529fa27a4
trangle <trangle@vooc.vn>
2017-12-29 08:58:18 +0700

    Change blog_film

--- static/assets/websites/js/blog_film.js
+++ static/assets/websites/js/blog_film.js
@@ -1,4 +1,22 @@
-$(document).ready(function(){
-	a = $("#blog-filter").val('getNewBlog');
-	console.log(a);
+//Filter News
+$('select#blog-filter').change(function() {
+	var filter = $(this).val()
+	filterList(filter);
 });
+
+//News filter function
+function filterList(value) {
+	var list = $(".blog-custom .blog-content");
+	$(list).fadeOut("fast");
+	if (value == "getNewBlog") {
+		$(".blog-custom").find("article").each(function (i) {
+			$(this).delay(100).slideDown("fast");
+		});
+	} else {
+		//Notice this *=" <- This means that if the data-category contains multiple options, it will find them
+		//Ex: data-category="Cat1, Cat2"
+		$(".blog-custom").find("article[data-category*=" + value + "]").each(function (i) {
+			$(this).delay(100).slideDown("fast");
+		});
+	}
+}
