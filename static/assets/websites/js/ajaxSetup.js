67b6600de58831504e4af29adb14f03929943f17
diemnguyen <diemnguyen@vooc.vn>
2018-01-02 15:19:33 +0700

    fix bugs fb comment

--- static/assets/websites/js/ajaxSetup.js
+++ static/assets/websites/js/ajaxSetup.js
@@ -29,20 +29,5 @@ $(document).ready(function() {
         }
     }); 
     $.ajaxSetup({ cache: false });
-    
-    // FB init function
-    FB.init({
-        appId: fbAppId,
-        version: 'v2.11',
-        cookie     : true,
-        xfbml      : true
-    });
 
-    (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/vi_VN/sdk.js';
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
 });
