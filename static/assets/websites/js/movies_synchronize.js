d30a3f469a4b9dc65410846499ae65e2a169017b
tien dang <tiendang@tiens-iMac.local>
2018-02-10 12:09:34 +0700

    add text show when success or fail

--- static/assets/websites/js/movies_synchronize.js
+++ static/assets/websites/js/movies_synchronize.js
@@ -7,11 +7,24 @@ $(document).ready(function() {
             crossDomain:false,
         })
         .done(function(response) {
+            $('#msg-sync').show();
+            setTimeout(function(){ 
+                
+                $('#msg-sync').hide();
+
+        }, 6000);
+           
            $('.msg-result-js').html(msgResult("Successfully", "success"));
         })
         .fail(function() {
             displayMsg();
             $('.msg-result-js').html(msgResult(error.responseJSON.message, "danger"));
+            $('#msg-sync-error').show();
+                setTimeout(function(){ 
+                    
+                    $('#msg-sync-error').hide();
+
+            }, 6000);
         });
     });
 });
