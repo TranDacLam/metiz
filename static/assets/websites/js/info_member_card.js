93b10cb167bed4ac751a33368f2cac3c51d3bc37
tranlam <tdlam2105@gmail.com>
2018-06-01 17:23:32 +0700

    UI transaction history, membercart, faqs

--- static/assets/websites/js/info_member_card.js
+++ static/assets/websites/js/info_member_card.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
-
     // active menu profile
     $('#info-member-card').addClass('active');
+
+    setBarcode();
+
+    function setBarcode(){
+        var settings = {
+            output: 'css',
+            bgColor: '#FFFFFF',
+            color: '#000000',
+            barWidth: '1',
+            barHeight: '50'
+        };
+        var type = 'code128';
+        var barcode = $('input[name=barcode_member_card]').val();
+
+        $('#barcode-member-card').barcode(
+            barcode, // Value barcode (dependent on the type of barcode)
+            type, // type (string)
+            settings
+        );
+    }
 
 });
