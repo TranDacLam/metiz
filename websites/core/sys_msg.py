ecc993e2f5d7c6ab3adf1e0948bc7f86fbd9f5a8
tien dang <tiendang@tiens-iMac.local>
2018-07-23 16:47:54 +0700

    add message global for project

---
+++ websites/core/sys_msg.py
@@ -0,0 +1,2 @@
+FORGOT_PASS_MSG_SMS_OTP = """Dat lai mat khau. Ma xac minh : %s"""
+MSG_SMS_OTP = """Quy khach dang thuc hien giao dich mua ve tai Metiz Cinema voi so tien la: %s. Ma xac nhan : %s"""
